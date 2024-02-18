9835f567f42724fc77bec39569c1a791e056ca17
Emma <emma.dawson@futurice.com>
2024-02-18 19:42:11 +0100

    Update accName page

--- src/pages/accname.tsx
+++ src/pages/accname.tsx
@@ -6,6 +6,18 @@ export default function AccNames() {
       <Header />
       <main className="main">
         <h1>Accessible Names</h1>
+        <div className="accNameContainer">
+          <button className="accNameBtn">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </button>
+          <button className="accNameBtn" aria-label="Navigation Toggle">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </button>
+        </div>
       </main>
     </>
   );
